f5a8e4d732694292e40b45d05fce01494049fa8a
rsgc-logush-o <ologush@rsgc.on.ca>
2017-02-28 13:41:40 -0500

    Initial Commit

---
+++ Football Match Predictor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Football Match Predictor
+//
+//  Created by Student on 2017-02-28.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Football Match Predictor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Football Match Predictor
+//
+//  Created by Student on 2017-02-28.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
