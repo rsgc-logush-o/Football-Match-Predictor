d310194efa761774c72ffa60236e8eae8dbeb9af
rsgc-logush-o <ologush@rsgc.on.ca>
2017-02-28 14:35:34 -0500

    Fixed my issue with the proposal. Also started working on the code.

--- Football Match Predictor/ViewController.swift
+++ Football Match Predictor/ViewController.swift
@@ -25,3 +25,40 @@ class ViewController: NSViewController {
 
 }
 
+struct team
+{
+    var name = ""
+    var wins = 0
+    var losses = 0
+    var ties = 0
+    var gamesPlayed = 0
+    
+    var pointsScored = 0
+    var pointsAgainst = 0
+    
+    
+}
+
+struct teamStats
+{
+    var meanPointsScored = 0
+    var meanPointsAgainst = 0
+    var medianPointsScored = 0
+    var medianPointsAgainst = 0
+    
+    var pointsScoredStdev = 0
+    var pointsAgainstStdev = 0
+    
+    
+}
+
+struct match
+{
+    var yourPoints = 0
+    var theirPoints = 0
+    
+    var isHome = 0
+    var result = 0
+    
+    
+}
