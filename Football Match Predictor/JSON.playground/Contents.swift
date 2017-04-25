13e4fde8a5f54425391fcb2b05d79d6f5a22fa3d
rsgc-logush-o <ologush@rsgc.on.ca>
2017-04-24 20:47:49 -0400

    Finished my JSON parsing and restructuring the data collection. The JSON is not working because I need to use the key I am trying to. I am getting an error from the let statement.

--- Football Match Predictor/JSON.playground/Contents.swift
+++ Football Match Predictor/JSON.playground/Contents.swift
@@ -2,21 +2,81 @@
 
 import Cocoa
 
+
+
+
+struct Match
+{
+    //Stores the opposing team in the match
+    var homeTeamID : Int
+    
+    //Stores the teams' score
+    var awayTeamID : Int
+    //Stores the opposing teams score for the match
+    var homeTeamScore : Float
+    
+    var awayTeamScore : Float
+    
+    var gameID : Int
+}
+
+struct TeamStats
+{
+    //The average points scored and average points allowed
+    var avgPointsScored : Float = 0
+    var avgPointsAgainst : Float = 0
+    
+    //These store the average overperformance/underperformance by the teams offense/defense
+    var avgPointsAgainstAvgDefense : Float = 0
+    var avgDefenseAgainstAvgPoints : Float = 0
+    
+}
+
+
+struct Team
+{
+    //Stores the team name
+    var name = ""
+    //Stores the wins losses and games played
+    var wins : Float = 0
+    var losses : Float = 0
+    var gamesPlayed : Float = 0
+    
+    //Stores the total points scored by the team, and points scored against the team
+    var pointsScored : Float = 0
+    var pointsAgainst : Float = 0
+    
+    //This array stores all of the matches played by this team
+    
+    //This stores the teams statistics in another structure
+    var stats : TeamStats
+    
+}
+
+
+var allMatches : [Match] = []
+
+var teams = [Int : Team]()
+
 func getTeamJSON()
 {
-    let adress : String = "insert string"
+    let adress : String = "https://api.fantasydata.net/v3/cfb/scores/JSON/Games/2016"
     
     if let url = URL(string: adress)
     {
         print(url)
         
         let urlRequest = URLRequest(url : url)
+        urlRequest.addValue("aacad0e238134cd180db37269fb8e684", forHTTPHeaderField: "Ocp-Apim-Subscription-Key")
         
         let config = URLSessionConfiguration.default
+        
         let session = URLSession(configuration : config)
+      
         
         let task = session.dataTask(with: urlRequest)
         {
+            
             (data, response, error)in
             
             if let r = response as? HTTPURLResponse {
@@ -72,9 +132,18 @@ func parseTeamJSON(_ theData : Data)
                     return
                 }
                 
-                var bufferTeam = Team(name: team, wins: wins, losses: losses, gamesPlayed: gamesPlayed, pointsScored: pointsFor, pointsAgainst: pointsAgainst, stats: TeamStats(avgPointsScored: pointsFor/gamesPlayed, avgPointsAgainst: pointsAgainst/gamesPlayed, avgPointsAgainstAvgDefense: 0, avgDefenseAgainstAvgPoints: 0))
+                let bufferTeam = Team(name: team, wins: wins, losses: losses, gamesPlayed: gamesPlayed, pointsScored: pointsFor, pointsAgainst: pointsAgainst, stats: TeamStats(avgPointsScored: pointsFor/gamesPlayed, avgPointsAgainst: pointsAgainst/gamesPlayed, avgPointsAgainstAvgDefense: 0, avgDefenseAgainstAvgPoints: 0))
                 
                 teams[teamID] = bufferTeam
+                
+                print(school)
+                print(team)
+                print(teamID)
+                print(wins)
+                print(losses)
+                print(pointsFor)
+                print(pointsAgainst)
+                print(gamesPlayed)
                 
             }
         }
@@ -88,13 +157,14 @@ func parseTeamJSON(_ theData : Data)
 
 func getScheduleJSON()
 {
-    let adress : String = "insert string"
+    let adress : String = "https://api.fantasydata.net/v3/cfb/scores/JSON/Games/2016"
     
     if let url = URL(string: adress)
     {
         print(url)
         
-        let urlRequest = URLRequest(url : url)
+        var urlRequest = URLRequest(url : url)
+         urlRequest.addValue("aacad0e238134cd180db37269fb8e684", forHTTPHeaderField: "Ocp-Apim-Subscription-Key")
         
         let config = URLSessionConfiguration.default
         let session = URLSession(configuration : config)
@@ -154,7 +224,7 @@ func parseScheduleJSON(_ theData : Data)
                         return
                 }
                 
-                var matchBuffer = Match(homeTeamID: homeTeamID, awayTeamID: awayTeamID, homeTeamScore: homeTeamScore, awayTeamScore: awayTeamScore, gameID: gameID)
+                let matchBuffer = Match(homeTeamID: homeTeamID, awayTeamID: awayTeamID, homeTeamScore: homeTeamScore, awayTeamScore: awayTeamScore, gameID: gameID)
                 
                 allMatches.append(matchBuffer)
                 
@@ -169,60 +239,8 @@ func parseScheduleJSON(_ theData : Data)
 }
 
 
-struct Match
-{
-    //Stores the opposing team in the match
-    var homeTeamID : Int
-    
-    //Stores the teams' score
-    var awayTeamID : Int
-    //Stores the opposing teams score for the match
-    var homeTeamScore : Float
-    
-    var awayTeamScore : Float
-    
-    var gameID : Int
-}
 
-struct TeamStats
-{
-    //The average points scored and average points allowed
-    var avgPointsScored : Float = 0
-    var avgPointsAgainst : Float = 0
-    
-    //These store the average overperformance/underperformance by the teams offense/defense
-    var avgPointsAgainstAvgDefense : Float = 0
-    var avgDefenseAgainstAvgPoints : Float = 0
-    
-}
-
-
-struct Team
-{
-    //Stores the team name
-    var name = ""
-    //Stores the wins losses and games played
-    var wins : Float = 0
-    var losses : Float = 0
-    var gamesPlayed : Float = 0
-    
-    //Stores the total points scored by the team, and points scored against the team
-    var pointsScored : Float = 0
-    var pointsAgainst : Float = 0
-    
-    //This array stores all of the matches played by this team
-    
-    //This stores the teams statistics in another structure
-    var stats : TeamStats
-    
-}
-
-
-
-var allMatches : [Match] = []
-
-var teams = [Int : Team]()
-
+getTeamJSON()
 
 
 
