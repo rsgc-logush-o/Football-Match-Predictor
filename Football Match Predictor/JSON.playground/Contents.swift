8f2c9c9fe3ea622ae2e5efc8835434ed51ee64da
rsgc-logush-o <ologush@rsgc.on.ca>
2017-04-26 13:31:19 -0400

    Finished my JSON parsing and restructuring the data collection. The JSON is not working because I need to use the key I am trying to use.

--- Football Match Predictor/JSON.playground/Contents.swift
+++ Football Match Predictor/JSON.playground/Contents.swift
@@ -7,16 +7,17 @@ import Cocoa
 
 struct Match
 {
-    //Stores the opposing team in the match
+    //Stores the hometeam's ID
     var homeTeamID : Int
     
-    //Stores the teams' score
+    //Stores the awayteam's ID
     var awayTeamID : Int
-    //Stores the opposing teams score for the match
-    var homeTeamScore : Float
     
+    //Stores the scores
+    var homeTeamScore : Float
     var awayTeamScore : Float
     
+    //Stores the gameID
     var gameID : Int
 }
 
@@ -46,34 +47,40 @@ struct Team
     var pointsScored : Float = 0
     var pointsAgainst : Float = 0
     
-    //This array stores all of the matches played by this team
     
     //This stores the teams statistics in another structure
     var stats : TeamStats
     
 }
 
-
+//This holds all of the matches from the season
 var allMatches : [Match] = []
 
+//This stores all of the teams, it is a dictionary so the team's ID will reveal the team
 var teams = [Int : Team]()
 
+//This gets all of the teams' data
 func getTeamJSON()
 {
+    //This is the adress to access the teams' data
     let adress : String = "https://api.fantasydata.net/v3/cfb/scores/JSON/Games/2016"
     
+    //Checking if this is a legitimate url
     if let url = URL(string: adress)
     {
         print(url)
         
-        let urlRequest = URLRequest(url : url)
+        //Creating the url request
+        var urlRequest = URLRequest(url : url)
+        //adding the api key
         urlRequest.addValue("aacad0e238134cd180db37269fb8e684", forHTTPHeaderField: "Ocp-Apim-Subscription-Key")
         
+        //setting the configuration to default
         let config = URLSessionConfiguration.default
         
         let session = URLSession(configuration : config)
       
-        
+        //beginning the session to collect data
         let task = session.dataTask(with: urlRequest)
         {
             
@@ -97,8 +104,9 @@ func getTeamJSON()
 
         }
         
+        //Resumes the rest of the code if task is not successful
         task.resume()
-        
+        //if the url is not valid this will happen
     } else {
         print("Error cannot create URL object")
     }
@@ -106,6 +114,7 @@ func getTeamJSON()
     
 }
 
+//This parses the teams' JSON data
 func parseTeamJSON(_ theData : Data)
 {
     print("-------------THIS IS THE DATA--------------")
@@ -113,10 +122,13 @@ func parseTeamJSON(_ theData : Data)
     do{
         let allTeams = try JSONSerialization.jsonObject(with: theData, options: JSONSerialization.ReadingOptions.allowFragments) as! [AnyObject]
         
+        //Iterating through all of the teams
         for eachTeam in allTeams
         {
+            //creating a dictionary for the data to be accessed from
             if let thisTeam = eachTeam as? [String : AnyObject]
             {
+                //Stores the needed data using let statements
                 guard let teamID : Int = thisTeam["TeamID"] as? Int,
                     let key : Int = thisTeam["Key"] as? Int,
                     let school : String = thisTeam["School"] as? String,
@@ -132,10 +144,13 @@ func parseTeamJSON(_ theData : Data)
                     return
                 }
                 
+                //This buffer team stores all of the team data
                 let bufferTeam = Team(name: team, wins: wins, losses: losses, gamesPlayed: gamesPlayed, pointsScored: pointsFor, pointsAgainst: pointsAgainst, stats: TeamStats(avgPointsScored: pointsFor/gamesPlayed, avgPointsAgainst: pointsAgainst/gamesPlayed, avgPointsAgainstAvgDefense: 0, avgDefenseAgainstAvgPoints: 0))
                 
+                //creates a location in the dictionary for the team using the teamID
                 teams[teamID] = bufferTeam
                 
+                //Prints all of the data obtained
                 print(school)
                 print(team)
                 print(teamID)
@@ -147,7 +162,7 @@ func parseTeamJSON(_ theData : Data)
                 
             }
         }
-    }catch let error as NSError {
+    }catch let error as NSError {//if it fails and there is an error
         
         print ("Failed to load: \(error.localizedDescription)")
         
@@ -155,20 +170,27 @@ func parseTeamJSON(_ theData : Data)
 
 }
 
+//This gets the JSON data for the schedule
 func getScheduleJSON()
 {
+    //This is the adress to access the data
     let adress : String = "https://api.fantasydata.net/v3/cfb/scores/JSON/Games/2016"
     
+    //creates url from string
     if let url = URL(string: adress)
     {
         print(url)
         
+        //creates the url request
         var urlRequest = URLRequest(url : url)
+        //adds the api key to the url request
          urlRequest.addValue("aacad0e238134cd180db37269fb8e684", forHTTPHeaderField: "Ocp-Apim-Subscription-Key")
         
+        //setting the default configuration
         let config = URLSessionConfiguration.default
         let session = URLSession(configuration : config)
         
+        //This gets the data
         let task = session.dataTask(with: urlRequest)
         {
             (data, response, error)in
@@ -200,15 +222,20 @@ func getScheduleJSON()
 
 }
 
+//This parses the JSON for the schedule
 func parseScheduleJSON(_ theData : Data)
 {
     do{
+        
         let allGames = try JSONSerialization.jsonObject(with: theData, options: JSONSerialization.ReadingOptions.allowFragments) as! [AnyObject]
         
+        //Iterates through each game in the schedule
         for eachGame in allGames
         {
+            //Casts into a dictionary to access the data
             if let thisGame = eachGame as? [String : AnyObject]
             {
+                //uses let statements to store the data
                 guard let gameID : Int = thisGame["GameID"] as? Int,
               let homeTeam : String = thisGame["HomeTeam"] as? String,
                let awayTeam : String = thisGame["AwayTeam"] as? String,
@@ -219,13 +246,15 @@ func parseScheduleJSON(_ theData : Data)
                 
                 
                 
-                    else {
+                    else {//If the data cannot be cast to these datatypes
                     print("Can't get data")
                         return
                 }
                 
+                //Temporarily hold the match data together in this buffer
                 let matchBuffer = Match(homeTeamID: homeTeamID, awayTeamID: awayTeamID, homeTeamScore: homeTeamScore, awayTeamScore: awayTeamScore, gameID: gameID)
                 
+                //This adds the buffer to all of the matches
                 allMatches.append(matchBuffer)
                 
             }
@@ -239,7 +268,7 @@ func parseScheduleJSON(_ theData : Data)
 }
 
 
-
+//Testing to get the team JSON
 getTeamJSON()
 
 
